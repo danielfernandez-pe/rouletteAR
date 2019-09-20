3ff3e904489053047ad4519cbce5909bc3727196
Daniel Fernandez Yopla <dyopla@techona.com>
2019-09-20 11:57:21 +0200

    Roulette and ball spining

---
+++ ARKitTest/SlotNumber.swift
@@ -0,0 +1,54 @@
+//
+//  SlotNumber.swift
+//  ARKitTest
+//
+//  Created by Daniel Gustavo Fernandez Yopla on 20/09/2019.
+//  Copyright © 2019 Daniel Gustavo Fernandez Yopla. All rights reserved.
+//
+
+import Foundation
+
+struct SlutNumber {
+    let number: Int
+    let degrees: Float
+}
+
+let numbers: [SlutNumber] = [
+    SlutNumber(number: 0, degrees: 0),
+    SlutNumber(number: 32, degrees: 9.72),
+    SlutNumber(number: 15, degrees: 19.45),
+    SlutNumber(number: 19, degrees: 29.18),
+    SlutNumber(number: 4, degrees: 38.91),
+    SlutNumber(number: 21, degrees: 48.64),
+    SlutNumber(number: 2, degrees: 58.37),
+    SlutNumber(number: 25, degrees: 68.10),
+    SlutNumber(number: 17, degrees: 77.83),
+    SlutNumber(number: 34, degrees: 87.56),
+    SlutNumber(number: 6, degrees: 97.29),
+    SlutNumber(number: 27, degrees: 107.02),
+    SlutNumber(number: 13, degrees: 116.75),
+    SlutNumber(number: 36, degrees: 126.48),
+    SlutNumber(number: 11, degrees: 136.21),
+    SlutNumber(number: 30, degrees: 145.94),
+    SlutNumber(number: 8, degrees: 155.67),
+    SlutNumber(number: 23, degrees: 165.40),
+    SlutNumber(number: 10, degrees: 175.13),
+    SlutNumber(number: 5, degrees: 184.86),
+    SlutNumber(number: 24, degrees: 194.59),
+    SlutNumber(number: 16, degrees: 204.32),
+    SlutNumber(number: 33, degrees: 214.05),
+    SlutNumber(number: 1, degrees: 223.78),
+    SlutNumber(number: 20, degrees: 233.51),
+    SlutNumber(number: 14, degrees: 243.24),
+    SlutNumber(number: 31, degrees: 252.97),
+    SlutNumber(number: 9, degrees: 262.70),
+    SlutNumber(number: 22, degrees: 272.43),
+    SlutNumber(number: 18, degrees: 282.16),
+    SlutNumber(number: 29, degrees: 291.89),
+    SlutNumber(number: 7, degrees: 301.62),
+    SlutNumber(number: 28, degrees: 311.35),
+    SlutNumber(number: 12, degrees: 321.08),
+    SlutNumber(number: 35, degrees: 330.81),
+    SlutNumber(number: 3, degrees: 340.54),
+    SlutNumber(number: 26, degrees: 350.27),
+]
---
+++ ARKitTest/ViewController.swift
@@ -0,0 +1,267 @@
+//
+//  ViewController.swift
+//  ARKitTest
+//
+//  Created by Daniel Gustavo Fernandez Yopla on 19/09/2019.
+//  Copyright © 2019 Daniel Gustavo Fernandez Yopla. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    // Nodes
+    var rouletteNode: SCNNode!
+    var ballNode: SCNNode!
+    var ballNodeParent: SCNNode!
+    
+    // General rotation properties
+    var isRotating = false
+    var totalSlotsInRoulette = 37.0
+    var totalSecondsToSpin = 15.0
+    var additionalSpeedToAdd = 0.01
+    
+    // Rotation properties for roulette
+    var rouletteCurrentAngleY: Float = 0
+    var rouletteCurrentSecondsSpinning = 0.0
+    var rouletteSecondsPerSpin = 0.5
+    var rouletteSecondsPerSlot: TimeInterval = 0.0
+    
+    // Rotation properties for roulette
+    var ballCurrentAngleY: Float = 0
+    var ballCurrentSecondsSpinning = 0.0
+    var ballSecondsPerSpin = 0.5
+    var ballSecondsPerSlot: TimeInterval = 0.0
+    var ballInRoulette = false
+
+    // Timers
+    var rouletteTimer: Timer!
+    var ballTimer: Timer!
+    
+//    var
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSceneView()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(gesture:)))
+        sceneView.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+
+    @IBAction func spinPressed(_ sender: Any) {
+        guard rouletteNode != nil,
+            !isRotating else { return }
+        dropBall()
+        startRotation()
+    }
+    
+    @objc
+    func handleTap(gesture: UITapGestureRecognizer) {
+        guard rouletteNode == nil else { return }
+        let sceneView = gesture.view as! ARSCNView
+        let touchLocation = gesture.location(in: sceneView)
+        
+        let hitResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        
+        if !hitResult.isEmpty {
+            addRoulette(hitResult: hitResult)
+            addBall()
+        }
+    }
+    
+    private func setupSceneView() {
+        sceneView.debugOptions = [.showFeaturePoints]
+        sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    private func addRoulette(hitResult: [ARHitTestResult]) {
+        guard let rouletteScene = SCNScene(named: "wood.usdz"),
+            let node = rouletteScene.rootNode.childNode(withName: "roulette", recursively: false),
+            let firstResult = hitResult.first else { return }
+        
+        rouletteNode = node
+        _ = rouletteNode.childNodes.map({ $0.scale = SCNVector3(0.03, 0.03, 0.03) })
+        let height = rouletteNode.boundingBox.max.y - rouletteNode.boundingBox.min.y
+        rouletteNode.position = SCNVector3(firstResult.worldTransform.columns.3.x, firstResult.worldTransform.columns.3.y + height / 2, firstResult.worldTransform.columns.3.z)
+        sceneView.scene.rootNode.addChildNode(rouletteNode)
+        createBallNodeParent()
+    }
+    
+    private func createBallNodeParent() {
+        ballNodeParent = SCNNode()
+        ballNodeParent.position = rouletteNode.position
+        sceneView.scene.rootNode.addChildNode(ballNodeParent)
+    }
+    
+    private func addBall() {
+        let ballGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.purple
+        ballGeometry.materials = [material]
+        ballNode = SCNNode(geometry: ballGeometry)
+        ballNode.position = SCNVector3(0.05, 0.05, 0)
+        ballNodeParent.addChildNode(ballNode)
+    }
+    
+    private func resetValues() {
+        isRotating = false
+        rouletteCurrentSecondsSpinning = 0.0
+        ballCurrentSecondsSpinning = 0.0
+        rouletteSecondsPerSpin = 0.5
+        ballSecondsPerSpin = 0.5
+        ballTimer.invalidate()
+        rouletteTimer.invalidate()
+    }
+    
+}
+
+// Roulette Rotation
+
+extension ViewController {
+    
+    private func startRotation() {
+        isRotating = true
+        rouletteSecondsPerSlot = TimeInterval(rouletteSecondsPerSpin / totalSlotsInRoulette)
+        ballSecondsPerSlot = TimeInterval(ballSecondsPerSpin / totalSlotsInRoulette)
+        setupTimerAndRotateForRoulette(timeInterval: rouletteSecondsPerSlot)
+        setupTimerAndRotateForBall(timeInterval: ballSecondsPerSlot)
+    }
+    
+    private func setupTimerAndRotateForRoulette(timeInterval: TimeInterval) {
+        rouletteTimer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(rotateRoulette), userInfo: nil, repeats: false)
+    }
+    
+    private func slotIsInFinalPosition() -> Bool {
+        // the current rouletteCurrentAngleY.radiansToDegrees is in the degrees position of the selected number from the array? Plus/minus 4 (range error)
+        // the ball should be in the same position, but we can check something else for the ball here. Let's try finds
+        return true
+    }
+    
+    @objc
+    func rotateRoulette() {
+        if rouletteCurrentSecondsSpinning >= totalSecondsToSpin && slotIsInFinalPosition() {
+            resetValues()
+            return
+        }
+        
+        print(rouletteCurrentAngleY.radiansToDegrees)
+        if rouletteCurrentAngleY.radiansToDegrees >= 360 {
+            rouletteCurrentAngleY = 0.0
+        }
+        
+        rouletteCurrentSecondsSpinning += rouletteSecondsPerSlot
+        
+        let anglePerSlot: Float = Float(360 / totalSlotsInRoulette)
+        rouletteCurrentAngleY += anglePerSlot.degreesToRadians
+        rouletteNode.eulerAngles.y = rouletteCurrentAngleY
+        
+        rouletteSecondsPerSpin += additionalSpeedToAdd
+        rouletteSecondsPerSlot = TimeInterval(rouletteSecondsPerSpin / totalSlotsInRoulette)
+        setupTimerAndRotateForRoulette(timeInterval: rouletteSecondsPerSlot)
+    }
+    
+}
+
+// Ball Rotation
+
+extension ViewController {
+    
+    private func dropBall() {
+        guard !ballInRoulette else { return }
+        ballInRoulette = true
+        let newPosition = SCNVector3(0.02, -0.06, 0)
+        let dropBallAction = SCNAction.move(by: newPosition, duration: 0.3)
+        ballNode.runAction(dropBallAction)
+    }
+    
+    private func setupTimerAndRotateForBall(timeInterval: TimeInterval) {
+        ballTimer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(startRotationForBall), userInfo: nil, repeats: false)
+    }
+    
+    @objc
+    func startRotationForBall() {
+        if ballCurrentSecondsSpinning >= totalSecondsToSpin && slotIsInFinalPosition() {
+            resetValues()
+            return
+        }
+        
+        print(ballCurrentAngleY.radiansToDegrees)
+        if ballCurrentAngleY.radiansToDegrees >= 360 {
+            ballCurrentAngleY = 0.0
+        }
+        
+        ballCurrentSecondsSpinning += ballSecondsPerSlot
+        
+        let anglePerSlot: Float = Float(360 / totalSlotsInRoulette)
+        ballCurrentAngleY -= anglePerSlot.degreesToRadians
+        ballNodeParent.eulerAngles.y = ballCurrentAngleY
+        
+        ballSecondsPerSpin += additionalSpeedToAdd
+        ballSecondsPerSlot = TimeInterval(ballSecondsPerSpin / totalSlotsInRoulette)
+        setupTimerAndRotateForBall(timeInterval: ballSecondsPerSlot)
+    }
+    
+}
+
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let plane = SCNPlane(width: width, height: height)
+        plane.materials.first?.diffuse.contents = UIColor(red: 0, green: 0, blue: 1, alpha: 0.8)
+        let planeNode = SCNNode(geometry: plane)
+        
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x,y,z)
+        planeNode.eulerAngles.x = -.pi / 2
+        planeNode.physicsBody = SCNPhysicsBody(type: .kinematic, shape: SCNPhysicsShape(geometry: plane, options: nil))
+        planeNode.physicsBody?.isAffectedByGravity = false
+        
+        node.addChildNode(planeNode)
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+    }
+
+}
+
+extension Float {
+    var degreesToRadians: Float { return self * .pi / 180 }
+    var radiansToDegrees: Float { return self * 180 / .pi }
+}
